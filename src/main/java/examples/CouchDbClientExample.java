bd8a1639d89c60042c0b76afd82d04f94c6c04de
Stephan H. Wissel <stw@linux.com>
2024-11-11 14:56:37 +0800

    Update CouchDbClientExample.java

--- src/main/java/examples/CouchDbClientExample.java
+++ src/main/java/examples/CouchDbClientExample.java
@@ -1,5 +1,30 @@
 package examples;
 
 public class CouchDbClientExample {
-  // TODO: implement
+
+  public void create(Vertx vertx) {
+    WebClient webClient = WebClient.create(vertx);
+    CouchdbClientBuilder builder = new CouchdbClientBuilder(vertx, webClient);
+    CouchdbClient couchDbClient = builder.build();
+  }
+
+  public void createAuthenticated(Vertx vertx, Credentials credentials) {
+    WebClient webClient = WebClient.create(vertx);
+    CouchdbClientBuilder builder = new CouchdbClientBuilder(vertx, webClient);
+    builder.credentials(credentials);
+    CouchdbClient couchDbClient = builder.build();
+  }
+
+  public void createFull(Vertx vertx, Credentials credentials, int port, String host, boolean isTls) {
+    WebClient webClient = WebClient.create(vertx);
+    CouchdbClient couchDbClient = new CouchdbClientBuilder(vertx, webClient)
+      .credentials(credentials)
+      .host(host)
+      .port(port)
+      .https(isTls)
+      .build();
+  }
+
+  
+  // TODO: implement more
 }
