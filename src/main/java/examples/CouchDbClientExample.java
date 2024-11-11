a66459286fc837abf9d320ff3d3de19d58d24c77
Stephan H. Wissel <stw@linux.com>
2024-11-11 07:37:48 +0000

    imports for example

--- src/main/java/examples/CouchDbClientExample.java
+++ src/main/java/examples/CouchDbClientExample.java
@@ -1,4 +1,20 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
 package examples;
+
+import io.vertx.ext.auth.authentication.Credentials;
+import io.vertx.ext.couchdb.CouchdbClient;
+import io.vertx.ext.couchdb.CouchdbClientBuilder;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.core.Vertx;
 
 public class CouchDbClientExample {
 
@@ -15,16 +31,17 @@ public class CouchDbClientExample {
     CouchdbClient couchDbClient = builder.build();
   }
 
-  public void createFull(Vertx vertx, Credentials credentials, int port, String host, boolean isTls) {
+  public void createFull(Vertx vertx, Credentials credentials, int port, String host,
+      boolean isTls) {
     WebClient webClient = WebClient.create(vertx);
     CouchdbClient couchDbClient = new CouchdbClientBuilder(vertx, webClient)
-      .credentials(credentials)
-      .host(host)
-      .port(port)
-      .https(isTls)
-      .build();
+        .credentials(credentials)
+        .host(host)
+        .port(port)
+        .https(isTls)
+        .build();
   }
 
-  
+
   // TODO: implement more
 }
